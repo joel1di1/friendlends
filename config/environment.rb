05e399df2393fc28c11afaa8af27b753eb2b9e3c
Benoit Lafontaine <joel1di1@gmail.com>
2010-06-10 02:46:11 +0200

    On peut rendre un emprunt + debut d'internationnalisation

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "devise", :version => '1.0.7'
   config.gem "heroku", :version => '1.9.9'
+  config.gem "haml"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -39,5 +40,5 @@ Rails::Initializer.run do |config|
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
+  config.i18n.default_locale = :fr
 end
