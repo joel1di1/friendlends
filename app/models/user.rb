dadd7b8f023f1c8049700c8a42764d1eec3f4527
Benoit Lafontaine <joel1di1@gmail.com>
2010-07-22 02:09:23 +0200

    on ne chope que les attributs qu'il faut

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,27 +8,13 @@ class User < ActiveRecord::Base
   
   has_many :loans
   
-  def after_facebook_connect(fb_session)
-
+  def before_facebook_connect(fb_session)
+    
     fb_session.user.populate(:locale, :current_location, :username, :name, :first_name, :last_name,
-                              :birthday_date, :sex, :city, :state, :country, :email)
+                              :birthday_date, :sex, :city, :state, :country)
+                              
+    self.username           = fb_session.user.name
 
-    self.locale             = my_fancy_locale_parser(fb_session.user.locale)
-    self.time_zone          = fb_session.user.current_location.try(:city)
-    self.country            = fb_session.user.current_location.try(:country)
-
-    self.username           = fb_session.user.username
-    self.email              = fb_session.user.email
-
-    self.profile.real_name  = fb_session.user.name
-    self.profile.first_name = fb_session.user.first_name
-    self.profile.last_name  = fb_session.user.last_name
-    self.profile.birthdate  = fb_session.user.birthday_date.try(:to_date)
-    self.profile.gender     = my_fancy_gender_parser(fb_session.user.sex)
-
-    self.profile.city       = fb_session.user.hometown_location.try(:city)
-    self.profile.zip_code   = fb_session.user.hometown_location.try(:state)
-    self.profile.country    = fb_session.user.hometown_location.try(:country)
 
   end
 end
