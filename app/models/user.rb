dd71f87ba08ce58e24c7ec3c13d1e13e75b17d53
Benoit Lafontaine <joel1di1@gmail.com>
2010-07-25 19:37:25 +0200

    recuperation depuis facebook UNIQUEMENT de first_name + last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ActiveRecord::Base
   
   def before_facebook_connect(fb_session)
     
-    fb_session.user.populate(:locale, :current_location, :username, :name, :first_name, :last_name,
-                              :birthday_date, :sex, :city, :state, :country)
+    fb_session.user.populate(:first_name, :last_name)
 
     self.username           = fb_session.user.first_name + " " + fb_session.user.last_name
   end
