07781fbedb5a034336132bafe6ebd41e5023afde
Benoit Lafontaine <joel1di1@gmail.com>
2010-07-25 17:47:39 +0200

    Ajout d'une rediection si on tape pas sur le site en www

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,9 +2,15 @@ class HomesController < ApplicationController
   layout "public"
   
   def index
-    respond_to do |format|
-      format.html unless user_signed_in?
-      format.html { redirect_to(loans_url) }
+    
+    if (request.url.include? 'www') || (request.url.include? 'localhost')
+      respond_to do |format|
+        format.html unless user_signed_in?
+        format.html { redirect_to(loans_url) }
+      end 
+    else
+      redirect_to 'http://www.ouestmaperceuse.com'
     end
+    
   end
 end
